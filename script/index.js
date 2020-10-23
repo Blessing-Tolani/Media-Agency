72754d17ab59f4f360198df2653954504ebf38f2
BlessingTolani <65096374+Blessing-Tolani@users.noreply.github.com>
2020-10-23 08:45:29 +0100

    Made the home page responsive,created a semantic artwork

--- script/index.js
+++ script/index.js
@@ -5,12 +5,13 @@ const navSlide = () => {
 
     burger.addEventListener("click", () => {
         nav.classList.toggle("nav-active");
+        
         navlinks.forEach((link,index) => {
             if(link.style.animation){
                 link.style.animation = '';
             }
             else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index/5 + 0.7}s`
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/5 + 0.7}s`
             }
         })
 
@@ -21,4 +22,8 @@ const navSlide = () => {
     
 }
 navSlide();
+
+
+
+
 
